29eb34e3d4e48bf778b7dbdd603810fa5dfb0fce
Adnaan Janees <adnaanjanees0@gmail.com>
2024-10-06 22:54:26 +0530

    Imported images and setup the Project structure

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const MyProfile = () => {
   return (
     <div>
-      
+        
     </div>
   )
 }
 
-export default App
+export default MyProfile
