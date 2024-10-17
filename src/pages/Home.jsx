03716e2395167926a7a7a5103f63901e4a99acf5
Adnaan Janees <adnaanjanees0@gmail.com>
2024-10-17 19:32:42 +0530

    Footer section completed Added custom fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import SpecialtyMenu from '../components/SpecialtyMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
 
+
 const Home = () => {
   return (
     <div>
